9ae634457dc16227cdec4b298187edc5c80d3a79
zeinab <49367777+zeinab-khj@users.noreply.github.com>
2026-07-26 15:50:25 +0330

    Create dataset.py

---
+++ src/dataset.py
@@ -0,0 +1,35 @@
+import torch
+from torch.utils.data import Dataset
+
+class IntentDataset(Dataset):
+
+    def __init__(
+        self,
+        texts,
+        labels,
+        tokenizer,
+        max_length=64
+    ):
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+
+        encoded = self.tokenizer(
+            self.texts[idx],
+            padding="max_length",
+            truncation=True,
+            max_length=self.max_length,
+            return_tensors="pt"
+        )
+
+        return {
+            "input_ids": encoded["input_ids"].squeeze(0),
+            "attention_mask": encoded["attention_mask"].squeeze(0),
+            "labels": torch.tensor(self.labels[idx])
+        }
